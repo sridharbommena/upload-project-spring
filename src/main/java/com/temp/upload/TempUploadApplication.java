822ef09740f15e44defff207aa5e692f2abc0921
sridharbommena <smartboysridhar@gmail.com>
2022-07-27 18:33:58 +0530

    excluded datasource

--- src/main/java/com/temp/upload/TempUploadApplication.java
+++ src/main/java/com/temp/upload/TempUploadApplication.java
@@ -6,11 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TempUploadApplication {
 
     @Autowired
